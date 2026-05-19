fbaaef10951f3f1d9ee29612d1be26779cb2d351
Lucas Piscicelli <lucaspiscicelli051@gmail.com>
2026-05-19 20:05:48 -0300

    segundo video

--- fullcomp/src/app/product-list/product-list.component.ts
+++ fullcomp/src/app/product-list/product-list.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-list',
   standalone: true,
+   imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
-  product = {
+  products: Product[] = [
+  {
     name: 'Iphone 16 Pro',
     make: 'Apple',
     price: 1500,
     stock: 5,
     image: '/img/16pro.jpeg'
-  };
+  },
+    {
+    name: 'A21',
+    make: 'Samsung',
+    price: 1000,
+    stock: 0,
+    image: '/img/a21.jfif'
+  },
+    {
+    name: 'Iphone 17 Pro',
+    make: 'Apple',
+    price: 2500,
+    stock: 2,
+    image: '/img/iphone.17pro.jfif'
+  }
+];
+constructor() { }
 
+ngOnInit(): void {
+}
 }
